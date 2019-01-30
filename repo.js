98c24d5c73dd3328006ddf6c75d5db8750d57ab3
Arandui <arandui42@gmail.com>
2019-01-30 17:20:05 +0100

    Finaler Test

--- repo.js
+++ repo.js
@@ -2,7 +2,8 @@
 	"contact": "arandui42@gmail.com",
 	"id": "Arandui",
 	"patches": {
-		"pc98_bgm": "PC98 BGM für TH06, TH07, TH08, TH09 und TH095"
+		"pc98_bgm": "PC98 BGM für TH06, TH07, TH08, TH09 und TH095",
+		"th07_ut_bgm": "TH07 mit Undertale BGM"
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/Arandui/trash_patch/master/"
---
+++ th07_ut_bgm/patch.js
@@ -0,0 +1,12 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order"
+	],
+	"id": "th07_ut_bgm",
+	"servers": [
+		"https://raw.githubusercontent.com/Arandui/trash_patch/master/th07_ut_bgm/"
+	],
+	"title": "TH07 mit Undertale BGM"
+}
