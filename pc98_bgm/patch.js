13063131739b8c062aca05ad00419286a3c4bf98
Arandui <arandui42@gmail.com>
2019-01-30 15:37:14 +0100

    Test 3

--- pc98_bgm/patch.js
+++ pc98_bgm/patch.js
@@ -6,7 +6,7 @@
 	],
 	"id": "pc98_bgm",
 	"servers": [
-		"https://github.com/Arandui/trash_patch.git/pc98_bgm/"
+		"https://raw.githubusercontent.com/Arandui/trash_patch/master/pc98_bgm/"
 	],
 	"title": "PC98 BGM für TH06, TH07, TH08, TH09 und TH095"
 }
