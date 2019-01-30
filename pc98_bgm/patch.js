911b4f0c12e81bcd6514ec163fea43c35ed50b62
Arandui <arandui42@gmail.com>
2019-01-30 14:21:30 +0100

    Test

---
+++ pc98_bgm/patch.js
@@ -0,0 +1,12 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order"
+	],
+	"id": "pc98_bgm",
+	"servers": [
+		"https://github.com/Arandui/trash_patch.git/pc98_bgm/"
+	],
+	"title": "PC98 BGM für TH06, TH07, TH08, TH09 und TH095"
+}
---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+	"contact": "arandui42@gmail.com",
+	"id": "Arandui",
+	"patches": {
+		"pc98_bgm": "PC98 BGM für TH06, TH07, TH08, TH09 und TH095"
+	},
+	"servers": [
+		"https://github.com/Arandui/trash_patch.git"
+	],
+	"title": "Trash Ideas"
+}
