b3ac896e3f0eddb8ee858644c9e3a089bac7a9ef
Arandui <arandui42@gmail.com>
2019-05-07 19:45:40 +0200

    Dateien für TH17 BGM Mod

---
+++ th17_doom_bgm/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "th17_doom_bgm",
+	"servers": [
+		"https://raw.githubusercontent.com/Arandui/trash_patch/master/th17_doom_bgm/"
+	],
+	"title": "Ersetzt die Musik mit Liedern aus Doom, Doom 2 und Final Doom TNT"
+}
