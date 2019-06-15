3767dba8adaf5fb34431cd514565928263a7e2db
Arandui <arandui42@gmail.com>
2019-06-16 00:26:18 +0200

    Neue BGM Mod für TH13

--- repo.js
+++ repo.js
@@ -5,6 +5,7 @@
 		"pc98_bgm": "PC98 BGM für TH06-9.5",
 		"pc98_reimu": "Ändert Reimus Aussehen zu PC98 (TH06, TH07 WIP)",
 		"th07_ut_bgm": "TH07 mit Undertale BGM",
+		"th13_spirit_bgm": "Ersetzt die normalen Tracks durch die Spirit-Versionen",
 		"th17_doom_bgm": "Ersetzt die Musik mit Liedern aus Doom, Doom 2 und Final Doom TNT",
 		"unhöfliche_okuu": "Macht Okuu unhöflich (TH11)"
 	},
---
+++ th13_spirit_bgm/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "th13_spirit_bgm",
+	"servers": [
+		"https://raw.githubusercontent.com/Arandui/trash_patch/master/th13_spirit_bgm/"
+	],
+	"title": "Ersetzt die normalen Tracks durch die Spirit-Versionen"
+}
